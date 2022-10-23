cb1cd781251836efe01fb47d52be3b2d50d045c8
bmichalczuk <barmic90@hotmail.com>
2022-10-23 09:09:50 +0200

    add styles to header and footer, add products page

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,14 +1,13 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-
-const AboutPage = () => {
+const ProductsPage = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="px-4 py-2 flex-grow max-w-md mx-auto w-full">abouuut</div>
+      <div className="px-4 py-2 flex-grow max-w-md mx-auto w-full">products</div>
       <Footer />
     </div>
   );
 };
 
-export default AboutPage;
+export default ProductsPage;
