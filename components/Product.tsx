dce5c3b39dbb15d00095ba3c3e6c0f222ab92e68
bmichalczuk <barmic90@hotmail.com>
2022-10-23 10:26:20 +0200

    add product component

---
+++ components/Product.tsx
@@ -0,0 +1,16 @@
+import { StoreApiResponse } from "../pages/products"
+export const Product = ({
+    title,
+    rating, 
+    image,
+    description
+} : StoreApiResponse) => {
+    console.log(image)
+    return (<li>
+        <div className="font-bold text-2xl">{title}</div>
+        <img className="border-4 border-orange-700 w-auto" src={image} alt={title} />
+        <div>{description}</div>
+        <div className="text-red-400">rating: {rating.rate}</div>
+
+    </li>)
+}
--- pages/products.tsx
+++ pages/products.tsx
@@ -1,6 +1,8 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { InferGetStaticPropsType } from "next";
+import { Product } from "../components/Product";
+import { title } from "process";
 const ProductsPage = ({
   data,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
@@ -8,7 +10,13 @@ const ProductsPage = ({
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="px-4 py-2 flex-grow max-w-md mx-auto w-full">
-        <div>{data[0]?.title}</div>
+        <div>
+          <ul>
+            {data.map((product) => {
+              return <Product {...product}/>;
+            })}
+          </ul>
+        </div>
       </div>
       <Footer />
     </div>
