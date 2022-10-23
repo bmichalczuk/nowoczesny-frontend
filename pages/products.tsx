26679e9c3b32b405210f25ea56258963bdf9825f
bmichalczuk <barmic90@hotmail.com>
2022-10-23 09:53:45 +0200

    add functions to handle api response in products

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,13 +1,42 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-const ProductsPage = () => {
+import { InferGetStaticPropsType } from "next";
+const ProductsPage = ({
+  data,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="px-4 py-2 flex-grow max-w-md mx-auto w-full">products</div>
+      <div className="px-4 py-2 flex-grow max-w-md mx-auto w-full">
+        <div>{data[0]?.title}</div>
+      </div>
       <Footer />
     </div>
   );
 };
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://fakestoreapi.com/products/");
+  const data: StoreApiResponse[] = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export interface StoreApiResponse {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
 
 export default ProductsPage;
